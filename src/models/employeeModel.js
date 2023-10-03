958eba78135a6cac14e4709cfc00fa452646dc35
Kamila Queiroz <kamilapaqueiroz@gmail.com>
2023-10-02 21:05:23 -0300

    Final commit

--- src/models/employeeModel.js
+++ src/models/employeeModel.js
@@ -5,7 +5,7 @@ const employeeSchema = new Schema({
     id: {type: Number, required: true, unique: true, min_length: 3},
     firstName: {type: String, required: true},
     lastName: {type: String, required: true},
-    cpf: {type: String, unique: true},
+    cpf: {type: String, required: true, unique: true},
     email: {type: String, required: true, unique: true},
     password: {type: String, required: true, min_length: 8}
 })
